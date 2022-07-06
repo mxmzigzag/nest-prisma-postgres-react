eef218124197d31e658b73c80a5a281062a06b19
Max Zahorskyi <qzzik97@gmail.com>
2022-07-06 12:23:45 +0300

    added dev webpack react

--- server/src/banned-user/banned-user.module.ts
+++ server/src/banned-user/banned-user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { BannedUserController } from './banned-user.controller';
 import { BannedUserService } from './banned-user.service';
 
