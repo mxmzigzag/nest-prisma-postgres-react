32a53e23f26eed73c225e711566703e33a1d65d0
Max Zahorskyi <qzzik97@gmail.com>
2022-07-31 01:22:11 +0300

    become creator notification component

--- client/src/pages/myPosts.tsx
+++ client/src/pages/myPosts.tsx
@@ -6,13 +6,22 @@ import { useGetPostsByAuthorIdQuery } from "../store/api/post.api";
 import ProfileLayout from "../layouts/profile.layout";
 import Posts from "../features/Posts/posts";
 import LoaderIcon from "../assets/svg/loader";
+import Button from "../components/ui/button";
 
 export default function MyPosts() {
   const { user } = useAuth();
 
   return (
     <ProfileLayout title="My posts">
-      {user?.id ? <MyPostsGrid userId={user.id} /> : <LoaderIcon />}
+      {user?.id ? (
+        user.role === "USER" ? (
+          <BecomeCreatorNotification />
+        ) : (
+          <MyPostsGrid userId={user.id} />
+        )
+      ) : (
+        <LoaderIcon />
+      )}
     </ProfileLayout>
   );
 }
@@ -24,4 +33,23 @@ const MyPostsGrid = ({ userId }: { userId: number }) => {
 
   // @ts-ignore
   return isLoading ? <LoaderIcon /> : <Posts posts={posts} gridColNum={3} />;
+};
+
+const BecomeCreatorNotification = () => {
+  const handleSendCreatorRequest = () => {
+    console.log("become a creator!");
+  };
+
+  return (
+    <div className="bcn-wrapper">
+      <h3 className="bcn-title">No posts here yet!</h3>
+      <p className="bcn-content">
+        To start writing your own articles you need to become a{" "}
+        <span className="bcn-highlight">creator</span>
+      </p>
+      <Button className="bcn-btn" onClick={handleSendCreatorRequest}>
+        Send a request
+      </Button>
+    </div>
+  );
 };
