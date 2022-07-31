e5f3acb1b08834e34a1db6e550c8097f0ca02f81
Max Zahorskyi <qzzik97@gmail.com>
2022-07-31 11:58:47 +0300

    added createdAt and updatedAt to some models, added status to request model

---
+++ server/prisma/migrations/20220731085815_init/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `BannedUser` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `status` to the `Request` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Request` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "RequestStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- AlterTable
+ALTER TABLE "BannedUser" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Request" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "status" "RequestStatus" NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
