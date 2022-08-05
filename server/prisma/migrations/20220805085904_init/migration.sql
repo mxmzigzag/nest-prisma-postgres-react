9ab033327c2fa778c71ba0ddff1b06ee1fb4a4b5
Max Zahorskyi <qzzik97@gmail.com>
2022-08-05 11:59:22 +0300

    changed id from number to uuid

---
+++ server/prisma/migrations/20220805085904_init/migration.sql
@@ -0,0 +1,93 @@
+/*
+  Warnings:
+
+  - The primary key for the `BannedUser` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Category` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Tag` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `TagsOnPosts` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "BannedUser" DROP CONSTRAINT "BannedUser_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Request" DROP CONSTRAINT "Request_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TagsOnPosts" DROP CONSTRAINT "TagsOnPosts_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TagsOnPosts" DROP CONSTRAINT "TagsOnPosts_tagId_fkey";
+
+-- AlterTable
+ALTER TABLE "BannedUser" DROP CONSTRAINT "BannedUser_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "BannedUser_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "BannedUser_id_seq";
+
+-- AlterTable
+ALTER TABLE "Category" DROP CONSTRAINT "Category_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Category_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Category_id_seq";
+
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "authorId" SET DATA TYPE TEXT,
+ALTER COLUMN "categoryId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Post_id_seq";
+
+-- AlterTable
+ALTER TABLE "Request" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Tag" DROP CONSTRAINT "Tag_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Tag_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Tag_id_seq";
+
+-- AlterTable
+ALTER TABLE "TagsOnPosts" DROP CONSTRAINT "TagsOnPosts_pkey",
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ALTER COLUMN "tagId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "TagsOnPosts_pkey" PRIMARY KEY ("postId", "tagId");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "BannedUser" ADD CONSTRAINT "BannedUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPosts" ADD CONSTRAINT "TagsOnPosts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPosts" ADD CONSTRAINT "TagsOnPosts_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
