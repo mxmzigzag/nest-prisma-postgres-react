06363eb96eeeea04628aea7ec73bd998a9de360d
Max Zahorskyi <qzzik97@gmail.com>
2022-09-16 00:44:29 +0300

    fixed navlink bug at postcard

--- client/src/features/Posts/postCard.tsx
+++ client/src/features/Posts/postCard.tsx
@@ -32,76 +32,73 @@ export default function PostCard({
   const [editIsOpen, setEditIsOpen] = useState(false);
 
   return (
-    <NavLink to={`/post/${id}`}>
-      <div
-        className={`post ${author?.banned ? "banned" : ""}`}
-        style={{
-          backgroundImage: `url(http://localhost:5000/${image})`,
-        }}
-      >
-        {tags ? <PostTags tags={tags} /> : null}
-        {isAuthor || isAdmin ? (
-          <>
-            <button
-              className="post-edit-btn"
-              onClick={(e) => {
-                e.preventDefault();
-                setEditIsOpen(true);
+    <div
+      className={`post ${author?.banned ? "banned" : ""}`}
+      style={{
+        backgroundImage: `url(http://localhost:5000/${image})`,
+      }}
+    >
+      {tags ? <PostTags tags={tags} /> : null}
+      {isAuthor || isAdmin ? (
+        <>
+          <button
+            className="post-edit-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              setEditIsOpen(true);
+            }}
+          >
+            <PenIcon color="#fff" width={20} height={20} />
+          </button>
+          <Modal
+            title="Edit Post"
+            isOpen={editIsOpen}
+            setIsOpen={setEditIsOpen}
+          >
+            <PostForm
+              post={{
+                id,
+                title,
+                image,
+                description,
+                authorId,
+                author,
+                categoryId,
+                category,
+                viewsCount,
+                tags,
               }}
-            >
-              <PenIcon color="#fff" width={20} height={20} />
-            </button>
-            <Modal
-              title="Edit Post"
-              isOpen={editIsOpen}
               setIsOpen={setEditIsOpen}
-            >
-              <PostForm
-                post={{
-                  id,
-                  title,
-                  image,
-                  description,
-                  authorId,
-                  author,
-                  categoryId,
-                  category,
-                  viewsCount,
-                  tags,
-                }}
-                setIsOpen={setEditIsOpen}
-              />
-            </Modal>
-          </>
+            />
+          </Modal>
+        </>
+      ) : null}
+      <div className="post-content">
+        {category ? (
+          <span
+            className="post-category"
+            style={{ backgroundColor: category.color }}
+            onClick={() => console.log(categoryId)}
+          >
+            {category.title}
+          </span>
         ) : null}
-        <div className="post-content">
-          {category ? (
-            <span
-              className="post-category"
-              style={{ backgroundColor: category.color }}
-              onClick={() => console.log(categoryId)}
-            >
-              {category.title}
-            </span>
-          ) : null}
-          <p className="post-title">{title}</p>
-          <p className="post-descr">{description}</p>
-        </div>
-        <div className="post-bottom">
-          {author ? (
-            <div className="post-author" onClick={() => console.log(authorId)}>
-              {author.username}
-              {author.banned ? (
-                <div className="banned-badge">Banned</div>
-              ) : null}
-            </div>
-          ) : null}
-          <div className="post-views">
-            <ViewsIcon className="post-views-icon" />
-            {viewsCount}
+        <p className="post-title">{title}</p>
+        <p className="post-descr">{description}</p>
+      </div>
+      <div className="post-bottom">
+        {author ? (
+          <div className="post-author" onClick={() => console.log(authorId)}>
+            {author.username}
+            {author.banned ? <div className="banned-badge">Banned</div> : null}
           </div>
+        ) : null}
+        <div className="post-views">
+          <ViewsIcon className="post-views-icon" />
+          {viewsCount}
         </div>
       </div>
-    </NavLink>
+      <NavLink to={`/post/${id}`} className="post-link" />
+    </div>
   );
 }
