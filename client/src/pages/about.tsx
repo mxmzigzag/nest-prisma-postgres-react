e5f7ba88d672aa7c1efc18eec7e9f8faed6917fc
Max Zahorskyi <qzzik97@gmail.com>
2022-09-16 21:59:21 +0300

    finishied about us

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -15,7 +15,10 @@ export default function About() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <img src="" alt="about" />
+        <img
+          src="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="about"
+        />
       </div>
     </PageLayout>
   );
